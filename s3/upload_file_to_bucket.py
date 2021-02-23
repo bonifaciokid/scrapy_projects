eea39af1e29fd3a3a348d4cec083f245ada3bdec
bonifaciokid <yamit.neilvincent1992@gmail.com>
2021-02-23 10:56:57 +0800

    make bucket_name as an argument

--- s3/upload_file_to_bucket.py
+++ s3/upload_file_to_bucket.py
@@ -1,12 +1,14 @@
 import boto3
 from botocore.client import Config
 
-def upload_image_to_s3(file_path, file_name):
+
+def upload_image_to_s3(file_path, file_name, bucket_name):
     """
         Upload image to s3 bucket.
         Args:
             file_path = image path from your local directory
             file_name = image file name saved from your pc
+            bucket_name = s3 bucket name
     """
 	try:
 		print ('Uploading image to s3 bucket')
@@ -16,7 +18,7 @@ def upload_image_to_s3(file_path, file_name):
 							aws_secret_access_key="aws_secret_key",
 							config=Config(signature_version='s3v4')
 						)
-		s3.meta.client.upload_file(file_path, 'bucket_name', file_name, ExtraArgs={"ContentType":'image/jpg', "ACL":'public-read'})
+		s3.meta.client.upload_file(file_path, bucket_name, file_name, ExtraArgs={"ContentType":'image/jpg', "ACL":'public-read'})
 		print ('image uploaded')
 	except OSError as ose:
 		print (ose)
